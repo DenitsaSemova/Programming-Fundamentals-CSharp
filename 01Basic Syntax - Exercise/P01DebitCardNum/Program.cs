185bb608f0fd8210d0752c84d1d44f8416261821
DenitsaSemova <112495370+DenitsaSemova@users.noreply.github.com>
2022-09-28 22:21:54 +0300

    Organizing files

---
+++ 01Basic Syntax - Exercise/P01DebitCardNum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int fourthNum = int.Parse(Console.ReadLine());
+
+            string debitCardNumber = $"{firstNum:d4} {secondNum:d4} {thirdNum:d4} {fourthNum:d4}";
+
+            Console.WriteLine(debitCardNumber);
+
+        }
+    }
+}
